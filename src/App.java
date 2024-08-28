31a1c11986491aa589d1c6703cf9c3c5762dcaa7
Sanderson <andreisanderson37@gmail.com>
2024-08-27 21:08:23 -0400

    Adicionando FakeDB

--- src/App.java
+++ src/App.java
@@ -1,8 +1,18 @@
 import visao.ClasseProdutoVisao;
+import visao.Produtovisao;
+import visao.SubclasseProdutoVisao;
 
 public class App {
     public static void main(String[] args) {
         ClasseProdutoVisao visao = new ClasseProdutoVisao();
+        SubclasseProdutoVisao subvisao = new SubclasseProdutoVisao();
+        Produtovisao prodvisao = new Produtovisao();
         visao.Exibir();
+        subvisao.ExibirSub();
+        prodvisao.ExibirProduto();
     }
 }
+
+
+
+
--- src/visao/ClasseProdutoVisao.java
+++ src/visao/ClasseProdutoVisao.java
@@ -1,26 +1,25 @@
 package visao;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 
 import dominio.ClasseProduto;
+import fakedb.ClasseProdutoFakeDB;
 
 public class ClasseProdutoVisao {
     public void Exibir(){
-        ClasseProduto cp1 = new ClasseProduto();
-        cp1.setCodigo(1);
-        cp1.setDescricao("Carnes");
-        cp1.setDataDeInclusao(LocalDate.now());
+        
+        ClasseProdutoFakeDB fakeDB = new ClasseProdutoFakeDB();
+        ArrayList<ClasseProduto> tabelaProduto = fakeDB.getTabela();
+        for (ClasseProduto cp : tabelaProduto) {
+            this.Imprimir(cp);
+        }
 
+    }
+    private void Imprimir(ClasseProduto alvo){
         System.out.println("Classe de produto: ");
-        System.out.println("Codigo: " + cp1.getCodigo());
-        System.out.println("Descrição: " + cp1.getDescricao());
-        System.out.println("Data de inclusão: " + cp1.getDataDeInclusao());
-
-        ClasseProduto cp2 = new ClasseProduto(2, "Bebidas", LocalDate.now());
-
-        System.out.println("Classe de produto: ");
-        System.out.println("Codigo: " + cp2.getCodigo());
-        System.out.println("Descrição: " + cp2.getDescricao());
-        System.out.println("Data de inclusão: " + cp2.getDataDeInclusao());
+        System.out.println("Código: " +alvo.getCodigo());
+        System.out.println("Descrição: " +alvo.getDescricao());
+        System.out.println("Data de inclusão: " +alvo.getDataDeInclusao());
+        System.out.println();
     }
 }
